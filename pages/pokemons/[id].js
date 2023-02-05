d98e009498a4e4df470a27b29765a39a42b3adec
Md.Mahabubul Kobir <96817149+MK-Sohan@users.noreply.github.com>
2023-02-05 16:48:46 +0600

    d

--- pages/pokemons/[id].js
+++ pages/pokemons/[id].js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import Layout from "../../components/Layout";
 
@@ -5,86 +6,140 @@ const Details = ({ pokeman, styles }) => {
   console.log(pokeman);
   return (
     <Layout title={pokeman.name.english}>
-      <div className="min-h-screen pt-8 flex flex-wrap sm:flex-nowrap justify-center mx-auto">
-        <div className="flex-1">
-          <img src={pokeman.image.hires} alt="" />
+      <div className="bg-white">
+        <div className="flex justify-center pt-10 ">
+          <Link href="/">
+            <a className="index">
+              <img
+                className=""
+                src="https://i.ibb.co/mHdPk2N/Logo.png"
+                alt=""
+                height={96}
+                width={264}
+              />
+            </a>
+          </Link>
         </div>
-        <div className="flex-1">
-          <p className="text-4xl font-semibold">
-            <span className="mr-4">#{pokeman.id}</span>
-            <span>{pokeman.name.english}</span>
-          </p>
-          <p>
-            <span className="text-sm">{pokeman.name.japanese}•</span>
-            <span className="text-sm">{pokeman.name.chinese}•</span>
-            <span className="text-sm">{pokeman.name.french}</span>
-          </p>
-          <p>
-            <span>Species:&nbsp;</span>
-            <span>{pokeman.species}</span>
-          </p>
-          <p>{pokeman.description}</p>
-          <p>
-            <span>Height:&nbsp;</span>
-            <span>{pokeman.profile.height}</span>
-          </p>
-          <p>
-            <span>Weight:&nbsp;</span>
-            <span>{pokeman.profile.weight}</span>
-          </p>
-          <p>
-            {pokeman.type.map((type, j) => {
-              return (
-                <span
-                  key={type}
-                  className="text-white text-2xl font-semibold mr-2 px-4 rounded"
-                  style={{ backgroundColor: styles[type.toLowerCase()] }}
-                >
-                  {type}
-                </span>
-              );
-            })}
-          </p>
+        <div className="min-h-screen pt-8 flex flex-wrap gap-x-20 sm:flex-nowrap justify-center mx-auto">
           <div>
-            {Object.keys(pokeman.base).map((stat, i) => {
-              let statPercentFactor = 0
-              let statColor
-              switch (stat) {
-                case "HP":
-                  statPercentFactor = 2.55;
-                  statColor = "#da4343";
-                  break;
-                case "Attack":
-                  statPercentFactor = 1.81;
-                  statColor = "#f38d45";
-                  break;
-                case "Defense":
-                  statPercentFactor = 2.3;
-                  statColor = "#f3d14a";
-                  break;
-                case "Sp. Attack":
-                  statPercentFactor = 1.73;
-                  statColor = "#547fe4";
-                  break;
-                case "Sp. Defense":
-                  statPercentFactor = 2.3;
-                  statColor = "#84df57";
-                  break;
-                case "Speed":
-                  statPercentFactor = 2.0;
-                  statColor = "#f75887";
-                  break;
-              }
-              return <div key={stat}>
-                <div className="flex justify-between">
-                  <span>{stat.toUpperCase()}</span>
-                  <span>{pokeman.base[stat]}</span>
-                </div>
-                <div className="h-4 w-full bg-gray-900 rounded-full">
-                  <div className="h-4 rounded-full" style={{backgroundColor:statColor, width:parseInt(pokeman.base[stat])*statPercentFactor}}></div>
-                </div>
-              </div>;
-            })}
+            <p className="text-4xl text-[#257BC4] font-semibold">
+              <span>{pokeman.name.english}</span>
+              <span className="ml-2 ">#{pokeman.id}</span>
+            </p>
+
+            {/* <p>
+              <span>Species:&nbsp;</span>
+              <span>{pokeman.species}</span>
+            </p> */}
+            <p className="w-[424px] font-normal mt-[32px] mb-[64px]">
+              {pokeman.description}
+            </p>
+            <div className=" clip bg-gray-100 w-[365px] flex justify-center py-10  gap-x-20 ">
+              <div>
+                <p>
+                  <span className="font-semibold ">Height&nbsp;</span>
+                  <br />
+                  <span className="font-normal mt-[16px]">
+                    {pokeman.profile.height}
+                  </span>
+                </p>
+                <p className="pt-10">
+                  <span className="font-semibold ">Weight:&nbsp;</span> <br />
+                  <span className="font-normal mt-[16px]">
+                    {pokeman.profile.weight}
+                  </span>
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span className="font-semibold ">Height&nbsp;</span>
+                  <br />
+                  <span className="font-normal mt-[16px]">
+                    {pokeman.profile.height}
+                  </span>
+                </p>
+                <p className="pt-10">
+                  <span className="font-semibold ">Weight:&nbsp;</span> <br />
+                  <span className="font-normal mt-[16px]">
+                    {pokeman.profile.weight}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <img src={pokeman.image.hires} alt="" />
+          </div>
+          <div className="">
+            <p className="font-semibold text-[20px] tracking-wider">Type:</p>
+            <p className="mt-[10px] font_family">
+              {pokeman.type.map((type, j) => {
+                return (
+                  <span
+                    key={type}
+                    className="text-white text-lg font-normal  px-3 py-1 rounded-sm"
+                    style={{ backgroundColor: styles[type.toLowerCase()] }}
+                  >
+                    {type}
+                  </span>
+                );
+              })}
+            </p>
+            <p className="font-semibold text-[20px] tracking-wider mt-3 mb-3">
+              Stats
+            </p>
+            <div>
+              {Object.keys(pokeman.base).map((stat, i) => {
+                let statPercentFactor = 0;
+                let statColor;
+                switch (stat) {
+                  case "HP":
+                    statPercentFactor = 2.55;
+                    statColor = "#da4343";
+                    break;
+                  case "Attack":
+                    statPercentFactor = 1.81;
+                    statColor = "#f38d45";
+                    break;
+                  case "Defense":
+                    statPercentFactor = 2.3;
+                    statColor = "#f3d14a";
+                    break;
+                  case "Sp. Attack":
+                    statPercentFactor = 1.73;
+                    statColor = "#547fe4";
+                    break;
+                  case "Sp. Defense":
+                    statPercentFactor = 2.3;
+                    statColor = "#84df57";
+                    break;
+                  case "Speed":
+                    statPercentFactor = 2.0;
+                    statColor = "#f75887";
+                    break;
+                }
+                return (
+                  <div className="mt-4" key={stat}>
+                    <div className="flex gap-y-4">
+                      <span className="font-semibold">
+                        {stat.toUpperCase()}
+                      </span>
+                      {/* <span>{pokeman.base[stat]}</span> */}
+                    </div>
+                    <div className="h-1 mt-2 w-[343px] bg-gray-900 rounded-full">
+                      <div
+                        className="h-1 rounded-full"
+                        style={{
+                          backgroundColor: statColor,
+                          width:
+                            parseInt(pokeman.base[stat]) * statPercentFactor,
+                        }}
+                      ></div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
